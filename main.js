d014c78364ad01ad8bf966a06a42b31897ccd803
Arathi Vallipuranathan <avallipuranathan@gmail.com>
2025-01-02 21:10:30 -0500

    create initial pages

--- main.js
+++ main.js
@@ -1,22 +1,28 @@
-
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
 
 let mainWindow;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
     width: 500,
-    height: 500,
+    height: 400,
     webPreferences: {
       nodeIntegration: true,
     },
   });
 
-  mainWindow.loadFile('index.html');
-  Menu.setApplicationMenu(null);
+  mainWindow.loadFile(path.join(__dirname, 'renderer/timer.html'));
 });
 
-
+// handle switching between pages
+ipcMain.on('navigate', (e, page) => {
+  if (page === 'setup') {
+    mainWindow.loadFile(path.join(__dirname, 'renderer/setup.html'));
+  } else if (page === 'timer') {
+    mainWindow.loadFile(path.join(__dirname, 'renderer/timer.html'));
+  }
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
